2f027ebbc904768182015cac7c99946f84f471b7
Jexxie <1989wjx@gmail.com>
2019-05-23 10:35:13 +0800

    add animation

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-import About from "./views/About.vue";
+import Team from "./views/Team.vue";
 import SignIn from "./views/SignInFlow/SignIn";
 import Request from "./views/SignInFlow/Request";
 import Recover from "./views/SignInFlow/Recover";
@@ -11,16 +11,15 @@ Vue.use(Router);
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: "/",
       name: "home",
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      component: About
+      path: "/team",
+      name: "team",
+      component: Team
     },
     {
       path: "/signin",
